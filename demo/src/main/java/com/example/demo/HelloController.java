906176b618e0312fb7ee6e71eaceb35886e424d2
tomoki-suruga <tomoki.s.0927@gmail.com>
2021-05-08 17:30:05 +0900

    add service

--- demo/src/main/java/com/example/demo/HelloController.java
+++ demo/src/main/java/com/example/demo/HelloController.java
@@ -3,11 +3,18 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.service.DemoService;
+
 @Controller
 public class HelloController {
 
 @RequestMapping(value="/hello")
 private String hello(){
-return "/index.html";
+	return "/index.html";
+}
+@RequestMapping(value="/demo")
+private String callService(){
+	DemoService.execute();
+	return "/index.html";
 }
 }
---
+++ demo/src/main/java/com/example/demo/service/DemoService.java
@@ -0,0 +1,10 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService {
+	public static void execute(){
+		System.out.println("ログ出力");
+	}
+}
